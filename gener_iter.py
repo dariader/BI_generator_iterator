6a4b1180d777d694e3d39847a3b5c2bc50b5dc08
dariader <30576713+dariader@users.noreply.github.com>
2019-02-27 00:54:27 +0300

    fixed negative direction of numbers

--- gener_iter.py
+++ gener_iter.py
@@ -16,8 +16,18 @@ class generator(object):
         self.s = step
         self.finish = finish
 
+        self.ok_mode = False
+        self.neg_mode = False
+
+        if begin <= finish:
+            self.ok_mode = True
+        elif begin >= finish:
+            self.neg_mode = True
+
+
+
     def __iter__(self):
-        while self.i != self.finish:
+        while (self.ok_mode == True and self.i <= self.finish) or (self.neg_mode == True and self.i >= self.finish):
             yield self.i
             self.i += self.s
 
@@ -38,13 +48,22 @@ class iterator(object):
         self.current = begin
         self.finish = finish
         self.step = step
+        self.ok_mode = False
+        self.neg_mode = False
 
+        if begin <= finish:
+            self.ok_mode = True
+        elif begin >= finish:
+            self.neg_mode = True
+    
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current >= self.finish:
+        if (self.ok_mode == True and self.current >= self.finish) or (self.neg_mode == True and self.current <= self.finish):
             raise StopIteration
+
         else:
             self.current += self.step
             return self.current - self.step
+
