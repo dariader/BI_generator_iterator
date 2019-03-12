7132e94df2f168a9cb75cc955c5785da0fce078c
dariader <30576713+dariader@users.noreply.github.com>
2019-03-12 20:48:43 +0300

    Fixed according to pull request
    Generator is a function, added several fixes to improve similarity with built-it function

--- gener_iter.py
+++ gener_iter.py
@@ -1,66 +1,98 @@
-class generator(object):
-    def __init__(self, begin, finish, step):
-        if finish is None:
-            finish = begin
-            begin = 0
-        if step is None:
+def generator(*args):
+
+    if len(args) == 3:
+        begin, finish, step = list(args)
+
+    elif len(args) == 2:
+        begin, finish = list(args)
+        if finish >= begin:
             step = 1
-        if finish is None:
-            raise ValueError('No input data')
-
-        assert(isinstance(begin, int))
-        assert (isinstance(finish, int))
-        assert (isinstance(finish, int))
-
-        self.i = begin
-        self.s = step
-        self.finish = finish
-
-        self.ok_mode = False
-        self.neg_mode = False
-
-        if begin <= finish:
-            self.ok_mode = True
-        elif begin >= finish:
-            self.neg_mode = True
-
-
-
-    def __iter__(self):
-        while (self.ok_mode == True and self.i <= self.finish) or (self.neg_mode == True and self.i >= self.finish):
-            yield self.i
-            self.i += self.s
-
-class iterator(object):
-    def __init__(self, begin, finish, step):
-        if finish is None:
-            finish = begin
-            begin = 0
-        if step is None:
+        else:
+            step = -1
+    elif len(args) == 1:
+        finish = args[0]
+        begin = 0
+        if finish >= begin:
             step = 1
-        if finish is None:
-            raise ValueError('No input data')
+        else:
+            step = -1
+    else:
+        raise ValueError('Wrong data input error')
 
-        assert(isinstance(begin, int))
+    if begin == finish or step > (finish-begin):
+        raise ValueError('Wrong data input error')
+
+    assert (isinstance(begin, int))
+    assert (isinstance(finish, int))
+    assert (isinstance(finish, int))
+
+
+    current = begin
+    step = step
+    finish = finish
+
+    pos_mode = False
+    neg_mode = False
+
+    if begin <= finish and step > 0:
+        pos_mode = True
+    elif begin >= finish:
+        neg_mode = True
+        if len(args) < 3:
+            return []
+    elif begin <= finish and step < 0:
+        return []
+    while (pos_mode == True and current < finish) or (neg_mode == True and current > finish):
+        yield current
+        current += step
+
+
+class Iterator(object):
+    def __init__(self, *args):
+        if len(args) == 3:
+            begin, finish, step = list(args)
+
+        elif len(args) == 2:
+            begin, finish = list(args)
+            if finish >= begin:
+                step = 1
+            else:
+                step = -1
+        elif len(args) == 1:
+            finish = args[0]
+            begin = 0
+            if finish >= begin:
+                step = 1
+            else:
+                step = -1
+        else:
+            raise ValueError('Wrong data input error')
+        assert (isinstance(begin, int))
         assert (isinstance(finish, int))
         assert (isinstance(finish, int))
 
         self.current = begin
         self.finish = finish
         self.step = step
-        self.ok_mode = False
+        self.pos_mode = False
         self.neg_mode = False
 
         if begin <= finish:
-            self.ok_mode = True
+            self.pos_mode = True
         elif begin >= finish:
             self.neg_mode = True
-    
+
+        self.args = args
+
     def __iter__(self):
         return self
 
     def __next__(self):
-        if (self.ok_mode == True and self.current >= self.finish) or (self.neg_mode == True and self.current <= self.finish):
+
+        if (self.pos_mode == True and self.current >= self.finish) or (
+                self.neg_mode == True and self.current <= self.finish):
+            raise StopIteration
+        elif self.neg_mode == True and len(self.args) < 3:
             raise StopIteration
 
         else:
